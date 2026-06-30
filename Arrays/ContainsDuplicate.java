2d217cc8dcda56ce05ca262c87e98baafd1595ab
unknown <raghav98402gmail.com>
2026-06-30 21:25:01 +0530

    Contains Duplicate | Day 1

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        boolean ans = containsDuplicate(nums);
+        System.out.println(ans);
+    }
+}
